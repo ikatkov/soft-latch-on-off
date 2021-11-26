1a4d17b5523a666bdc063f8af1c8de0c4200d359
Igor Katkov <ikatkov@gmail.com>
2021-11-25 23:04:09 -0800

    logic update

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,40 @@
 #include <Arduino.h>
 #include <EasyButton.h>
 static const byte CAL_BUTTON_PIN = 2;
+static const byte LED_PIN = 13;
 
 EasyButton calButton(CAL_BUTTON_PIN);
+bool state;
 
-
-void onPressedForDuration() {
-    Serial.println("onPressedForDuration");
+void onPressedForDuration()
+{
+  Serial.println("onPressedForDuration");
+  pinMode(CAL_BUTTON_PIN, OUTPUT);
+  digitalWrite(CAL_BUTTON_PIN, LOW);
 }
 
-
-void onButtonPressed() {
-    Serial.println("onButtonPressed");
+void onButtonPressed()
+{
+  Serial.println("onButtonPressed");
 }
-
 
 void setup()
 {
+  calButton.begin();
+  calButton.onPressed(onButtonPressed);
+  calButton.onPressedFor(2000, onPressedForDuration);
+  pinMode(LED_PIN, OUTPUT);
   Serial.begin(9600);
   while (!Serial)
     delay(10);
 
   Serial.println("Setup");
-  calButton.begin();
-  calButton.onPressed(onButtonPressed);
-  calButton.onPressedFor(2000, onPressedForDuration
-  );
 }
 
 void loop()
 {
   calButton.read();
+  state = !state;
+  digitalWrite(13, state);
+  delay(500);
 }
